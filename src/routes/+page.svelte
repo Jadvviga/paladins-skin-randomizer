a6e497d73d48c3f30531e57f8896ab8dbe3e09cc
jadvviga <ster.jadwiga@gmail.com>
2023-10-21 22:34:43 +0200

    improved randomizing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,18 @@
 <h2>Welcome to Paladins skin randomizer</h2>
 <div id="rand_container">
-    {#if randSkin}
+    {#if randSkin && randBtnBorderColor === BORDER_DEFAULT}
         <h3>{randSkin.name} {randSkin.champion}</h3>
     {:else}
-        <h3>Click below to get random skin!</h3>
+        <h3>{text}</h3>
     {/if}
-    <button
-        class="rand_button btn"
-        on:click={getRandomSkin}>
-        
-        {#if randSkin}
-            <img class="rand_button rand_img" src={getFilePath(randSkin)} alt="Randomized champion">
-        {:else}
-            gimmie random skin!
-        {/if}
-    </button>
+    <img
+        class="rand_button rand_img"
+        style="--background-col:{randBtnBorderColor}"
+        on:click={randomizeSkin}
+        src={randSkin ? getFilePath(randSkin) : RAND_BTN_PATH}
+        alt="Randomized champion">
     
 </div>
-
-
-
 
 
 <script lang="ts">
@@ -28,12 +21,53 @@
     import type { Skin } from "../data/skinsData";
     import { page } from '$app/stores';
 
-    const  SKINS_COUNT = skins.length;
-    let randSkin: Skin;
+    const COLORS = ["red", "orange", "yellow", "yellowgreen", "green", "aqua", "dodgerblue", "blue", "indigo", "darkmagenta", "deeppink"]
+    const SKINS_COUNT = skins.length;
+    const BORDER_DEFAULT = "black";
+    const TEXT_DEFAULT = "Click below to get random skin!";
 
-    function getRandomSkin() {
-        const rand = Math.floor(Math.random() * SKINS_COUNT);
-        randSkin = skins[rand];
+    let randSkin: Skin;
+    let randNumber: number;
+    let randBtnBorderColor = BORDER_DEFAULT;
+    let colorCounter = 0;
+    let borderColorInterval = null;
+    let stopInterval = null;
+    let text = TEXT_DEFAULT;
+
+    const RAND_BTN_PATH = `${$page.url}/img/random_btn.png`
+
+    function setRandomSkin() {
+        randNumber = Math.floor(Math.random() * SKINS_COUNT);
+        while (skins[randNumber] === randSkin) {
+            randNumber = Math.floor(Math.random() * SKINS_COUNT);
+        }
+        
+    }
+    
+    function setBorderAndImg() {
+        randBtnBorderColor = COLORS[colorCounter];
+        console.log(randBtnBorderColor)
+        colorCounter++;
+        if (colorCounter >= COLORS.length) {
+            colorCounter = 0;
+        }
+
+        const r = Math.floor(Math.random() * SKINS_COUNT);
+        randSkin = skins[r];
+    }
+
+    function stopRandomizing() {
+        randBtnBorderColor = BORDER_DEFAULT;
+        randSkin = skins[randNumber];
+        window.clearInterval(borderColorInterval);
+        window.clearInterval(stopInterval)
+    }
+
+    function randomizeSkin() {
+        setRandomSkin();
+        text = "Randomizing..."
+        borderColorInterval = window.setInterval(setBorderAndImg, 100);
+        stopInterval = window.setInterval(stopRandomizing, 3000)
     }
 
     function getFilePath(skin: Skin): string {
@@ -45,11 +79,14 @@
 
 
 
+
 </script>
 
 <style>
 	#rand_container {
-		text-align: center;
+		display: flex;
+        flex-direction: column;
+        align-items: center;
 	}
 
     .rand_button {
@@ -57,10 +94,13 @@
         font-size: 5em;
         padding: 0;
         margin: 0;
-        width: 500px;
-        height: 500px;
+        width: 510px;
+        height: 510px;
         border-radius: 20px;
-        background-color: black;
+        background-color: var(--background-col);
+        display: block;
+        text-align: center;
+        padding: 5px;
     }
 
     .rand_button:hover {
@@ -68,11 +108,8 @@
     }
     .rand_button:active {
        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-       color: rgba(0, 0, 0, 0.5);
+       
     }
 
-    .rand_img {
-        display: block;
-    }
 
 </style>
